4fc28bc3b0c38ea7125196e684f76f50394181b9
Maksonchyks <musinmaksim28@gmail.com>
2024-11-16 19:19:15 +0200

    Make HomePage and NotFoundPage

---
+++ src/pages/NotFoundPage.Vue
@@ -0,0 +1,8 @@
+// src/pages/NotFoundPage.vue
+<template>
+  <v-container class="text-center">
+    <h1 class="text-h2 mb-6">404</h1>
+    <p class="text-h4 mb-6">Page Not Found</p>
+    <v-btn color="primary" to="/"> Go Home </v-btn>
+  </v-container>
+</template>
